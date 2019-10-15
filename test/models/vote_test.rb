f608755fb941a2c71546f55743168d1733dcc120
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-10-14 20:18:00 -0700

    refactored to use fixtures

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,21 +1,15 @@
 require "test_helper"
 
 describe Vote do
-  describe "relationships" do
-    before do
-      @user = User.create(username: "user", date_joined: Date.today)
-      @work = Work.create(category: "book", name: "To Kill A Mockingbird", creator: "Harper Lee")
-      @vote = Vote.create(user_id: @user.id, work_id: @work.id)
-    end
-    
+  describe "relationships" do    
     it "has a user" do
-      expect(@vote.user).wont_be_nil
-      expect(@vote.user).must_equal @user
+      vote = votes(:scalzi_october)
+      expect(vote.user).must_equal users(:scalzi)
     end
     
     it "has a work" do
-      expect(@vote.work).wont_be_nil
-      expect(@vote.work).must_equal @work
+      vote = votes(:scalzi_october)
+      expect(vote.work).must_equal works(:october)
     end
   end
 end
