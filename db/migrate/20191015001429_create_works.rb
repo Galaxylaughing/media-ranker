b250130288b089b5d7105529eca855281b425969
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-10-14 17:19:46 -0700

    work model migration

--- db/migrate/20191015001429_create_works.rb
+++ db/migrate/20191015001429_create_works.rb
@@ -1,12 +1,12 @@
 class CreateWorks < ActiveRecord::Migration[5.2]
   def change
     create_table :works do |t|
-      t.symbol :category
+      t.string :category
       t.string :name
       t.string :creator
       t.string :description
       t.date :published_date
-
+      
       t.timestamps
     end
   end
