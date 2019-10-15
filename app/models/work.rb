ab514789930a674243fa8eb6716d22d8aa4c288d
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-10-14 17:42:19 -0700

    added validations

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,9 @@
 class Work < ApplicationRecord
+  
+  # validations
+  validates :name, presence: true
+  # _should_ make sure all name-creator combinations must be unique
+  validates_uniqueness_of :name, scope: :creator
+  validates :category, presence: true
+  
 end
