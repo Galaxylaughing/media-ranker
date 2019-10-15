db3e2306d58c9543aa88dc0c1fffb061eae59d08
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-10-14 18:31:26 -0700

    tested work validations

--- app/models/work.rb
+++ app/models/work.rb
@@ -3,6 +3,7 @@ class Work < ApplicationRecord
   
   # validations
   validates :name, presence: true
+  validates :creator, presence: true
   # _should_ make sure all name-creator combinations must be unique
   validates_uniqueness_of :name, scope: :creator
   validates :category, presence: true
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,7 +1,44 @@
 require "test_helper"
 
 describe Work do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "validations" do
+    before do
+      @work = Work.new(category: "book", name: "To Kill A Mockingbird", creator: "Harper Lee")
+    end
+    
+    it "can pass validations" do
+      expect(@work.valid?).must_equal true
+    end
+    
+    it "is valid with a duplicate name but unique creator" do
+      new_book = Work.create(category: "book", name: @work.name, creator: "Some Other Author")
+      
+      expect(@work.valid?).must_equal true
+    end
+    
+    it "is invalid without a name" do
+      @work.name = nil
+      
+      expect(@work.valid?).must_equal false
+    end
+    
+    it "is invalid without a creator" do
+      @work.creator = nil
+      
+      expect(@work.valid?).must_equal false
+    end
+    
+    it "is invalid without a unique name-creator combination" do
+      new_work = Work.create(category: "book", name: @work.name, creator: @work.creator)
+      
+      expect(@work.valid?).must_equal false
+      expect(@work.errors.messages).must_include :name
+    end
+    
+    it "is invalid without a category" do
+      @work.category =  nil
+      
+      expect(@work.valid?).must_equal false
+    end
+  end
 end
