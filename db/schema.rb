d883a305f93e79694ff01e5d5e62aaf0b6890b7e
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-10-14 17:43:42 -0700

    foreign key for work id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_15_003525) do
+ActiveRecord::Schema.define(version: 2019_10_15_004240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2019_10_15_003525) do
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "work_id"
+    t.index ["work_id"], name: "index_votes_on_work_id"
   end
 
   create_table "works", force: :cascade do |t|
@@ -38,4 +40,5 @@ ActiveRecord::Schema.define(version: 2019_10_15_003525) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "votes", "works"
 end
