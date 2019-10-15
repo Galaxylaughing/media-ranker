b3e475d26c6dde80caa35909b9537bde2a254a1a
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-10-14 19:08:36 -0700

    tested vote relationships

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 describe Vote do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "relationships" do
+    before do
+      @user = User.create(username: "user", date_joined: Date.today)
+      @work = Work.create(category: "book", name: "To Kill A Mockingbird", creator: "Harper Lee")
+      @vote = Vote.create(user_id: @user.id, work_id: @work.id)
+    end
+    
+    it "has a user" do
+      expect(@vote.user).wont_be_nil
+      expect(@vote.user).must_equal @user
+    end
+    
+    it "has a work" do
+      expect(@vote.work).wont_be_nil
+      expect(@vote.work).must_equal @work
+    end
+  end
 end
