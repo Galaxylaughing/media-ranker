af9e01942070c920bb238d5ddfad22c1e4d2eb03
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-10-14 17:15:09 -0700

    work model

---
+++ db/migrate/20191015001429_create_works.rb
@@ -0,0 +1,13 @@
+class CreateWorks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :works do |t|
+      t.symbol :category
+      t.string :name
+      t.string :creator
+      t.string :description
+      t.date :published_date
+
+      t.timestamps
+    end
+  end
+end
