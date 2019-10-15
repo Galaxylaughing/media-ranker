f6e4ebb50b66524bacc68c164cc75fc00eaea703
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-10-14 17:44:48 -0700

    foreign key for user id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_15_004240) do
+ActiveRecord::Schema.define(version: 2019_10_15_004401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2019_10_15_004240) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "work_id"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
     t.index ["work_id"], name: "index_votes_on_work_id"
   end
 
@@ -40,5 +42,6 @@ ActiveRecord::Schema.define(version: 2019_10_15_004240) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "votes", "users"
   add_foreign_key "votes", "works"
 end
