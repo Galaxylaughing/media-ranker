d6447db2e1661ed8c6a84f04bdf00a1a73083bae
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-10-14 17:33:23 -0700

    user model

---
+++ db/migrate/20191015003236_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.datetime :date_joined
+
+      t.timestamps
+    end
+  end
+end
