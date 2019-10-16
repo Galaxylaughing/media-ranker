699d8d4d6b2e4d73038a81f4961aae9a2c94d9ba
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-10-16 14:47:53 -0700

    added validations for vote

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,7 @@
 class Vote < ApplicationRecord
   belongs_to :work
   belongs_to :user
+  
+  # _should_ make sure all usr-work combinations must be unique
+  validates_uniqueness_of :user_id, scope: :work_id
 end
--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -12,4 +12,42 @@ describe Vote do
       expect(vote.work).must_equal works(:october)
     end
   end
+  
+  describe "validations" do
+    before do
+      user = users(:metz)
+      work = works(:noteworthy)
+      @vote = Vote.new(user_id: user.id, work_id: work.id)
+    end
+    
+    it "can pass validations" do
+      expect(@vote.valid?).must_equal true
+    end
+    
+    it "is valid with a duplicate usr but unique work" do
+      new_work = works(:ophelia)
+      new_vote = Vote.create(user_id: @vote.user_id, work_id: new_work.id)
+      
+      expect(@vote.valid?).must_equal true
+    end
+    
+    it "is invalid without a user" do
+      @vote.user_id = nil
+      
+      expect(@vote.valid?).must_equal false
+    end
+    
+    it "is invalid without a work" do
+      @vote.work_id = nil
+      
+      expect(@vote.valid?).must_equal false
+    end
+    
+    it "is invalid without a unique work-usr combination" do
+      new_vote = Vote.create(user_id: @vote.user_id, work_id: @vote.work_id)
+      
+      expect(@vote.valid?).must_equal false
+      expect(@vote.errors.messages).must_include :user_id
+    end
+  end
 end
